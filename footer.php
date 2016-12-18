96007e9adfcccc5d27352b0d3c6140acce20fc6b
forkinthecode <budgetaus@hotmail.com>
2016-12-18 16:27:12 +1100

    code fixes for today

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+
+echo"
+<hr>
+<p>Code and database design by <a href='https://twitter.com/Info_Aus'>Rosie Williams</a> | <a href='https://github.com/forkinthecode/snitchhunt'>Source Code</a> |  Data provided by government |
+
+ Icons made by <a href='http://www.flaticon.com/authors/freepik'>Freepik</a> </p>";
+
+?>
