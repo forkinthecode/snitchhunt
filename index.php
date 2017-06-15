b787211036356996b2e1b25c813ee747050646be
forkinthecode <atcra@protonmail.com>
2017-06-15 12:50:32 +1000

    changed text on index page

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ include'framework.php';
 	<li><a href='https://www.letsgetsafe.org/' target='_blank'>Guided self help securing your devices</a></li>
   <li><a href='https://hackblossom.org/' target='_blank'>Hackblossom: 'DIY Cybersecurity for Domestic Violence'</a></li>
 	<li><a href='https://www.youtube.com/results?search_query=data+retention+australia' target='_blank'>Data Retention on YouTube</li>
-  <li><a href='https://www.ag.gov.au/dataretention' target='_blank'>Attorney-General's FAQ</a></li>
+  <li><a href='https://www.ag.gov.au/dataretention' target='_blank'>Attorney-General's Metadata Retention Scheme FAQ</a></li>
 	<li><a href='https://whistleblower.network/2016/11/18/privacychat-faq/' target='_blank'>Links of interest</a></li>
 	<li><a href='https://www.meetup.com/CryptoAus-Sydney/' target='_blank'>Events</a></li>
 
@@ -115,25 +115,10 @@ include'framework.php';
 </div>
 
 
-<button class="accordion">About</button>
-<div class="panel">
-<p><a href='http://www.abc.net.au/triplej/programs/hack/how-team-of-pre-teens-found-whisteblower-using-metadata/8113668' target='_blank'>SnitchHunt</a> is a game designed by security consultant, Gabor Szathmari to teach the public about how different sets
-of metadata can be brought together to identify &amp; track the movements of individuals.</p>
-<p>The original version of SnitchHunt was taken offline due to costs of hosting because it relied on <a href='https://en.wikipedia.org/wiki/Kibana'>Kibana</a> to allow 
-	users to search against the datasets. </p>
-	<p>It was for this reason that I decided to create a different version of the game that did not use Kibana so that 
-		it could be kept online indefinitely.  </p>
-<p>I also decided to add a simpler, quicker activity based on the SnitchHunt data that can be run in evening time-slots.</p>
-<p>The <a href='index.php#a9'>short version</a> is suitable for evening timeslots, the <a href='index.php#a10'>longer much harder version</a> is suitable for day-long events.</p>
-<p>Either version can be done online at your leisure, just follow the introductory slides through to get the gist.</p>
-<p>The easy version does not require log in and is not scored. The difficult version requires sign up to keep track of scoring so is a more competitive type of event.</p>
-<p>My version of SnitchHunt is currently open sourced at <a href='https://github.com/forkinthecode/snitchhunt'>GitHub</a>.</p>
-<p>Rosie Williams has a degree in Sociology (inequality, public policy) and programming skills in PHP, MySQL, CSS &amp; HTML. </p>
-<p>Images are from pixabay.com. Icons from flaticon. </p>
-</div>
 
 
-<button class="accordion">Contact</button>
+
+<button class="accordion">About/Hire me</button>
 <div class="panel">
   <p>
 <a href="https://twitter.com/Info_Aus" class="twitter-follow-button" data-show-count="false">Follow @Info_Aus</a>
@@ -141,10 +126,12 @@ of metadata can be brought together to identify &amp; track the movements of ind
   js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
      </p>
 
-<p><a href='https://whistleblower.network/contact'>Rosie Williams</a></p>
-<p><a href='https://twitter.com/gszathmari'>Gabor Szathmari</a> </p>
+<p>Contact Rosie at ATCRA@protonmail.com to hire me to run this workshop for your people.</p>
 
 
+<p>My version of SnitchHunt is currently open sourced at <a href='https://github.com/forkinthecode/snitchhunt'>GitHub</a>.</p>
+<p>Rosie Williams has a degree in Sociology (inequality, public policy) and programming skills in PHP, MySQL, CSS &amp; HTML. </p>
+<p>Images are from pixabay.com. Icons from flaticon. </p>
 
 	</div>
 
